7bf2c68c0fcb1bd3b2252c46c3ee761b32144271
Paolo <parker9694@gmail.com>
2026-01-08 22:09:08 +0100

    Sync to work on other PC

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,7 @@ const ast = std.zig.Ast;
 pub fn main() !void {
     var arena_allocator = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena_allocator.deinit();
-    injectLogCalls(arena_allocator.allocator(), std.Io.Dir.openDir()));
+    injectLogCalls(arena_allocator.allocator(), std.fs.openDirAbsolute("/home/Paolo/code/zig-playground", .{ .iterate = true }) catch |e| @panic(@errorName(e)));
 
     // var idx: [1]ast.Node.Index = undefined;
     // const function_prototype = tree.fnProto(@enumFromInt(1));
@@ -13,38 +13,67 @@ pub fn main() !void {
     // std.debug.print("Node's tag name is {s}.\n", .{fn_proto.?.ast.proto_node});
 }
 
-fn injectLogCalls(allocator: std.mem.Allocator, project_directory: std.fs.Dir) void {
-    const hash_map: std.StringHashMap(ast) = undefined;
-    const files = detecZigFiles(project_directory);
+fn injectLogCalls(allocator: std.mem.Allocator, project_directory: []u8) void {
+    // const hash_map: std.StringHashMap(ast) = undefined;
+    // const files = detecZigFiles(project_directory);
+
+    const f = detectZigFiles(allocator, project_directory);
+    for (0..f.items.len) |i| {
+        var tree = parseZigFile(allocator, f.items[i]) catch |e| @panic(@errorName(e));
+        defer tree.deinit(allocator);
+        getTokens(tree);
+    }
 }
 
-fn detectZigFiles(project_directory: std.fs.Dir) []std.fs.File {
-    const allocator = std.heap.page_allocator;
-    std.fs.Dir.walk(project_directory, allocator);
-}
+fn detectZigFiles(allocator: std.mem.Allocator, project_directory: std.fs.Dir) std.ArrayList([]u8) {
+    var zig_files = std.ArrayList([]u8).allocatedSlice();
+    var walker = std.fs.Dir.walk(project_directory, allocator) catch |e| @panic(@errorName(e));
+    defer walker.deinit();
 
-fn parseZigFiles() []ast {
-    const file = std.fs.cwd().openFile("/home/Paolo/code/zig-playground/example.zig", .{ .mode = .read_write }) catch @panic("error opening file");
-    var buf: [512]u8 = undefined;
-    const file_size = file.read(buf[0..]) catch @panic("error reading file");
+    var file_count: usize = 0;
+    var path_buf: [std.fs.max_path_bytes]u8 = undefined;
 
-    var gpa = std.heap.DebugAllocator(.{}).init;
-    buf[file_size] = 0;
-    const source: [:0]const u8 = buf[0..file_size :0];
-    const tree = try ast.parse(gpa.allocator(), source, .zig);
-}
+    while (walker.next() catch |e| @panic(@errorName(e))) |entry| {
+        const dir_path = entry.dir.realpath(".", &path_buf) catch |e| @panic(@errorName(e));
 
-fn getTokens(buf: []u8, tree: ast) []u8 {
-    std.debug.print("getTokens\n", .{});
-    var total_len: usize = 0;
-    for (0..tree.tokens.len) |i| {
-        const slice = tree.tokenSlice(@intCast(i));
-        tree.tokenStart())
-        @memmove(buf[total_len .. total_len + slice.len], slice);
-        total_len += slice.len;
+        const full_path = std.mem.concat(allocator, u8, &[_][]const u8{
+            dir_path,
+            &[_]u8{std.fs.path.sep},
+            entry.path,
+        }) catch |e| @panic(@errorName(e));
+
+        if (!std.mem.endsWith(u8, full_path, ".zig")) continue;
+
+        zig_files.append(allocator, full_path) catch |e| @panic(@errorName(e));
+        file_count += 1;
     }
 
-    // TODO: 
+    std.debug.print("{any}\n", .{zig_files});
+    return zig_files;
+}
+
+fn parseZigFile(allocator: std.mem.Allocator, file_path: []u8) !ast {
+    std.debug.print("Parsing file {any}\n", .{file_path});
+    var file = std.fs.openFileAbsolute(file_path, .{ .mode = .read_write }) catch |e| @panic(@errorName(e));
+    var buf: [512]u8 = undefined;
+    const file_size = try file.read(buf[0..]);
+    buf[file_size] = 0;
+    const source: [:0]const u8 = buf[0..file_size :0];
+    return try ast.parse(allocator, source, .zig);
+}
+
+fn getTokens(tree: ast) void {
+    std.debug.print("getTokens\n", .{});
+    _ = tree;
+    // var total_len: usize = 0;
+    // for (0..tree.tokens.len) |i| {
+    //     const slice = tree.tokenSlice(@intCast(i));
+    //     tree.tokenStart();
+    //     @memmove(buf[total_len .. total_len + slice.len], slice);
+    //     total_len += slice.len;
+    // }
+
+    // TODO:
     // 1. get token tags via tree.tokenTag()
     // 2. detect function declarations using the tags, you will have something like fn + identifier + open bracket + ...
     // 3. use tree.tokenStart() to get the byte offset of the function's open bracket { in the source code
@@ -52,5 +81,6 @@ fn getTokens(buf: []u8, tree: ast) []u8 {
     // 5. into the copied buffer, inject a log call that prints at least the identifier of the function you found at step 2
     // 6. save the file to the original location
 
-    return buf[0..total_len];
+    // return buf[0..total_len];
+    // return buf[0..1];
 }
