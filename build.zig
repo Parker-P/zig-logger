89805548346a617ed39b8bfe7f30d9a17012d3e4
parker9694@gmail.com <parker9694@gmail.com>
2026-01-17 23:52:56 +0100

    Re-added src subfolder

--- build.zig
+++ build.zig
@@ -6,7 +6,7 @@ pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
         .name = "autologger",
         .root_module = b.createModule(.{
-            .root_source_file = b.path("main.zig"),
+            .root_source_file = b.path("src/main.zig"),
             .target = target,
             .optimize = optimize,
         }),
