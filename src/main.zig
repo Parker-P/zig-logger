526611cc21fff3fae6f14308e494dbe3c9127d49
parker9694@gmail.com <parker9694@gmail.com>
2026-01-17 15:56:42 +0100

    Fixed bug when parsing parameter and return types

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const ast = std.zig.Ast;
+const TokenTag = std.zig.Token.Tag;
 
 const LogContext = struct {
     allocator: std.mem.Allocator,
@@ -48,11 +49,12 @@ pub fn main() !void {
     for (0..zig_file_paths.items.len) |i| {
         std.debug.print("Parsing file {s}\n", .{zig_file_paths.items[i]});
         var tree = parseZigFile(allocator, zig_file_paths.items[i]) catch |e| @panic(@errorName(e));
+        defer tree.deinit(allocator);
         var functions_info: std.ArrayList(LogContext) = .empty;
         defer functions_info.deinit(allocator);
         getFunctionsInfo(allocator, tree, zig_file_paths.items[i], &functions_info);
+        std.debug.print("Found {d} functions\n", .{functions_info.items.len});
         injectLogInstructions(allocator, functions_info, zig_file_paths.items[i]);
-        tree.deinit(allocator);
         formatZigFile(allocator, zig_file_paths.items[i]);
     }
 }
@@ -86,8 +88,8 @@ fn detectZigFiles(allocator: std.mem.Allocator, project_directory: []const u8) s
 }
 
 fn getFunctionsInfo(allocator: std.mem.Allocator, tree: ast, file_path: []u8, out_functions_info: *std.ArrayList(LogContext)) void {
-    // if (std.mem.containsAtLeast(u8, file_path, 1, "main.zig")) return;
-    const TokenTag = std.zig.Token.Tag;
+    // if (!std.mem.containsAtLeast(u8, file_path, 1, "dupe.zig")) return;
+
     var i: i32 = -1;
     while (i < tree.tokens.len - 1) {
         i += 1;
@@ -107,33 +109,43 @@ fn getFunctionsInfo(allocator: std.mem.Allocator, tree: ast, file_path: []u8, ou
         if (tag != TokenTag.l_paren) continue;
         i += 1;
 
-        while (tag != TokenTag.r_paren or tag != TokenTag.eof) {
+        while (tag != TokenTag.r_paren and tag != TokenTag.eof) {
+            i = parseFunctionParameter(tree, &i, &log_context);
             tag = tree.tokenTag(@intCast(i));
             if (tag == TokenTag.comma) {
                 i += 1;
                 tag = tree.tokenTag(@intCast(i));
             }
-            if (tag != TokenTag.identifier) break;
-            log_context.addParameterName(tree.tokenSlice(@intCast(i))) catch |e| @panic(@errorName(e));
-            i += 1;
-            tag = tree.tokenTag(@intCast(i));
-            if (tag != TokenTag.colon) break;
-            i += 1;
-            tag = tree.tokenTag(@intCast(i));
-            if (tag != TokenTag.identifier) break;
-            i += 1;
         }
-
         if (tag != TokenTag.r_paren) continue;
         i += 1;
         tag = tree.tokenTag(@intCast(i));
-        if (tag != TokenTag.identifier) continue;
-        i += 1;
-        tag = tree.tokenTag(@intCast(i));
+
+        while (tag != TokenTag.l_brace and tag != TokenTag.eof) {
+            i += 1;
+            tag = tree.tokenTag(@intCast(i));
+        }
+
         if (tag != TokenTag.l_brace) continue;
         log_context.byte_offset = @intCast(tree.tokenStart(@intCast(i)) + 1);
         out_functions_info.*.append(allocator, log_context) catch |e| @panic(@errorName(e));
     }
+}
+
+fn parseFunctionParameter(tree: ast, i: *i32, log_ctx: *LogContext) i32 {
+    var tag = tree.tokenTag(@intCast(i.*));
+    if (tag != TokenTag.identifier) return i.*;
+    log_ctx.*.addParameterName(tree.tokenSlice(@intCast(i.*))) catch |e| @panic(@errorName(e));
+    i.* += 1;
+    tag = tree.tokenTag(@intCast(i.*));
+    if (tag != TokenTag.colon) return i.*;
+    i.* += 1;
+    tag = tree.tokenTag(@intCast(i.*));
+    while (tag != TokenTag.r_paren and tag != TokenTag.eof and tag != TokenTag.comma) {
+        i.* += 1;
+        tag = tree.tokenTag(@intCast(i.*));
+    }
+    return i.*;
 }
 
 fn parseZigFile(allocator: std.mem.Allocator, file_path: []u8) !ast {
@@ -149,8 +161,10 @@ fn injectLogInstructions(allocator: std.mem.Allocator, log_ctxs: std.ArrayList(L
     for (0..log_ctxs.items.len) |i| {
         var buf: [256]u8 = undefined;
         const log_instruction = createLoggingInstruction(log_ctxs.items[i], &buf);
-        for (i + 1..log_ctxs.items.len) |j|
+        for (i + 1..log_ctxs.items.len) |j| {
             log_ctxs.items[j].byte_offset += log_instruction.len;
+            log_ctxs.items[j].function_location.line += 1;
+        }
         insertText(allocator, file_path, log_ctxs.items[i].byte_offset, log_instruction);
     }
 }
